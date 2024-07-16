bbe68534df78d75e44f3275f3c018d5626bdd89a
Anderson Moser <testesoftware2@siam.ind.br>
2024-07-16 14:02:22 -0300

    v2

--- cadastro_empresa.py
+++ cadastro_empresa.py
@@ -8,69 +8,86 @@ import time
 # Configurar o driver do Chrome
 driver = webdriver.Chrome()
 
-# Abrir o navegador e acessar a página
-driver.get('http://10.0.2.193:4000')
-driver.maximize_window()
+# Função para realizar login
+def login(driver):
+    driver.get('http://173.212.231.29:4002')
+    driver.maximize_window()
+    
+    usuario = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="username"]'))
+    )
+    senha = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="password"]')
+    
+    usuario.send_keys('09405185000199')
+    senha.send_keys('siam')
+    senha.send_keys(Keys.ENTER)
 
-# Identificar os campos de login
-usuario = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="username"]'))
-)
-senha = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="password"]')
+# Função para acessar o menu de cadastros
+def acessar_menu_cadastros(driver):
+    menucadastros = WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'app-menu > ul > li:nth-child(2) > a'))
+    )
+    menucadastros.click()
+    
+    submenuempresas = WebDriverWait(driver, 40).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="#/registration/companys"]'))
+    )
+    submenuempresas.click()
+    time.sleep(5)
 
-# Realizar login
-usuario.clear()
-usuario.send_keys('05573761900')
-senha.clear()
-senha.send_keys('791536aA')
-senha.send_keys(Keys.ENTER)
+# Função para criar uma nova empresa
+def criar_nova_empresa(driver):
+    novoempresa = WebDriverWait(driver, 40).until(
+        EC.element_to_be_clickable((By.XPATH, '//span[text()="Novo"]'))
+    )
+    novoempresa.click()
 
-# Aguardar até que o menu de cadastros esteja presente
-menucadastros = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'app-menu > ul > li:nth-child(2) > a'))
-)
-menucadastros.click()
+    uf = WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-child(5) > div.p-col-12.p-md-12 > div:nth-child(2) > p-dropdown > div > span'))
+    )
+    uf.click()
+    uf_option = WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.p-dropdown-items-wrapper.ng-tns-c64-43 > ul > p-dropdownitem:nth-child(24) > li'))
+    )
+    uf_option.click()
 
-# Aguardar até que o submenu de empresas esteja presente e clicar nele
-submenuempresas = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="#/registration/companys"]'))
-)
-submenuempresas.click()
+    razaosocial = WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="corporateName"]'))
+    )
+    cnpj = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="document"]')
+    emailempresa = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="email"]')
+    adicionaremailempresa = driver.find_element(By.CSS_SELECTOR, 'div:nth-child(9) > div:nth-child(3) > button')
+    
+    # Preenchendo os campos
+    razaosocial.send_keys('SEGURANÇA FORTE')
+    cnpj.send_keys('20237527000141')
+    emailempresa.send_keys('testesoftware2@siam.ind.br')
+    adicionaremailempresa.click()
 
-# Tentar localizar e clicar no botão "Novo" usando o texto do botão
-novoempresa = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((By.XPATH, '//span[text()="Novo"]'))
-)
-novoempresa.click()
+# Função para gravar empresa
+def gravar_empresa(driver):
+    gravarempresa = WebDriverWait(driver, 40).until(
+        EC.element_to_be_clickable((By.XPATH, '//span[text()="Gravar"]'))
+    )
+    gravarempresa.click()
 
-# Identificar os campos do formulário
-razaosocial = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="corporateName"]'))
-)
-cnpj = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="document"]')
-emailempresa = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="email"]')
-adicionaremailempresa = driver.find_element(By.CSS_SELECTOR, 'div:nth-child(9) > div:nth-child(3) > button')
+    try:
+        # Esperar pela mensagem de sucesso
+        cadastro_salvo = WebDriverWait(driver, 40).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, 'body > app-root > app-content-layout > div > div.layout-main > div > p-messages > div > div > div > span.p-message-summary.ng-tns-c124-2.ng-star-inserted'))
+        )
+        # Verificar o texto da mensagem de sucesso
+        assert "Registro gravado com sucesso!" in cadastro_salvo.text
+        print("Mensagem de cadastro realizado com sucesso!")
+    except Exception as e:
+        print("A mensagem não foi encontrada ou o texto não corresponde:", str(e))
 
-# Preenchendo os campos
-razaosocial.send_keys('SEGURANÇA FORTE')
-cnpj.send_keys('63647511000110')
-emailempresa.send_keys('testesoftware2@siam.ind.br')
-adicionaremailempresa.click()
-
-# Selecionar estado
-uf = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-child(5) > div.p-col-12.p-md-12 > div:nth-child(2) > p-dropdown > div > span'))
-)
-uf.click()
-uf_option = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'p-dropdownitem:nth-child(24) > li > span'))
-)
-uf_option.click()
-
-# Adicione qualquer ação adicional necessária aqui, como salvar o formulário
-
-# Aguarde alguns segundos antes de fechar o navegador (opcional)
-time.sleep(5)
-
-# Fechar o navegador
-driver.quit()
+# Execução do script
+try:
+    login(driver)
+    acessar_menu_cadastros(driver)
+    criar_nova_empresa(driver)
+    gravar_empresa(driver)
+finally:
+    # Fechar o navegador
+    driver.quit()
--- cadastro_user.py
+++ cadastro_user.py
@@ -13,7 +13,7 @@ driver.get('http://10.0.2.193:4000')
 driver.maximize_window()
 
 # Identificar os campos de login
-usuario = WebDriverWait(driver, 10).until(
+usuario = WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="username"]'))
 )
 senha = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="password"]')
@@ -26,7 +26,72 @@ senha.send_keys('791536aA')
 senha.send_keys(Keys.ENTER)
 
 # Aguardar até que o menu de cadastros esteja presente
-menucadastros = WebDriverWait(driver, 20).until(
+menucadastros = WebDriverWait(driver, 60).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, 'app-menu > ul > li:nth-child(2) > a'))
 )
 menucadastros.click()
+
+# Aguardar até que o submenu de empresas esteja presente e clicar nele
+submenuusuario = WebDriverWait(driver, 60).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="#/registration/users"]'))
+)
+submenuusuario.click()
+
+# Tentar localizar e clicar no botão "Novo" usando o texto do botão
+novousuario = WebDriverWait(driver, 60).until(
+    EC.element_to_be_clickable((By.XPATH, '//span[text()="Novo"]'))
+)
+novousuario.click()
+
+# Identificar os campos do formulário
+nome = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[formcontrolname="name"]'))
+)
+
+# Preenchendo os campos
+nome.send_keys('SEGURANÇA FORTE')
+
+# Selecionar classe de usuario
+classeusuario = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-child(3) > div:nth-child(4) > div:nth-child(2) > app-auto-complete > p-autocomplete > span > button > span.p-button-icon.pi.pi-chevron-down'))
+)
+classeusuario.click()
+classeusuario_option = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '#pr_id_6_list > li:nth-child(1)'))
+)
+classeusuario_option.click()
+
+# Selecionar tipo de chave
+tipodechave = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-child(20) > div:nth-child(1) > div.p-formgroup-inline > div.p-col-10.p-col-nogutter > app-auto-complete > p-autocomplete > span > button'))
+)
+tipodechave.click()
+tipodechave_option = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '#pr_id_9_list > li:nth-child(9)'))
+)
+tipodechave_option.click()
+
+# Selecionar marcador
+marcador = WebDriverWait(driver, 40).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div:nth-child(3) > div:nth-child(8) > div.p-formgroup-inline > div.p-col-10.p-col-nogutter > app-auto-complete > p-autocomplete > span > button'))
+        )
+marcador.click()
+
+marcador_option = WebDriverWait(driver, 40).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#pr_id_7_list > li:nth-child(1)'))
+        )
+marcador_option.click()
+
+# Anexar imagem 
+anexarimagem = WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="file"]'))
+)
+# Caminho do arquivo
+caminho_do_arquivo = r'C:\Users\anderson.moser\Pictures\imagens rosto\FACIAS SIAM\0000000001.jpg'
+
+# Enviar o caminho do arquivo para o input do tipo file
+anexarimagem.send_keys(caminho_do_arquivo)
+
+# Adicione qualquer ação adicional necessária aqui, como salvar o formulário
+
+
